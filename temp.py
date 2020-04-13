969580274359a1e395644fa3dd56767f4672771e
njcx <njcx86@gmail.com>
2020-04-13 13:07:29 +0800

    init

--- temp.py
+++ temp.py
@@ -6,29 +6,39 @@ from pyspark import SparkContext
 from pyspark import SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-from settings import KafkaParams, SSHGroupId, SSHTopic, CheckPointDir,NidsAlertTopic
+from settings import KafkaParams, SSHGroupId, SSHTopic, CheckPointDir, NidsAlertTopic
 from utils import json_to_py
 from utils import  KafkaTools
 
+
+rules = []
+
+
 def send_partition(iter):
+
     kafka_utils =KafkaTools(KafkaParams["metadata.broker.list"])
 
     for record in iter:
-        kafka_utils.produce(NidsAlertTopic,record)
+
+        kafka_utils.produce(NidsAlertTopic, record)
         # connection.send(record)
     # return to the pool for future reuse
     # ConnectionPool.returnConnection(connection)
-
-
-
-def
-
-
-
-
-
+#
+#
+# def line_check(line):
+#
+#
+# def rdd_check(rdd):
+#
+#     rdd_ = rdd.cache()
+#
+#     for rule in rules:
+#
+#         rdd_.filter(rule)
 
 # def json_ds():
+
 
 def create_context():
     sc_conf = SparkConf()
@@ -39,12 +49,10 @@ def create_context():
                                                kafkaParams=dict(KafkaParams, **{"group.id": SSHGroupId}))
     msg_stream.checkpoint(20)
     result = msg_stream.map(lambda x: json_to_py(x[1]))
-
-    result.foreachRDD(lambda rdd: rdd.foreachPartition(sendPartition))
-
-    result.pprint()
+    result.foreachRDD(lambda rdd: rdd.foreachPartition(send_partition))
     ssc.checkpoint(CheckPointDir)
     return ssc
+
 
 ssc = StreamingContext.getOrCreate(CheckPointDir, create_context)
 ssc.start()
