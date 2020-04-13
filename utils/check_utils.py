7c15c9cf138e7a22504b611e02636eaefe700c88
njcx <njcx86@gmail.com>
2020-04-13 17:06:18 +0800

    init

--- utils/check_utils.py
+++ utils/check_utils.py
@@ -4,24 +4,51 @@
 
 
 from utils.log_ import Logger
+
+try:
+    import re2 as re
+except ImportError:
+    import re
+
 logger = Logger.get_logger(__name__)
 
 
 def check_in((res, pattern)):
-    pass
+
+    try:
+        return pattern in res
+
+    except Exception as e:
+        logger.error(str(e))
+        return False
 
 
 def check_equal(res, pattern):
-
-    pass
+    try:
+        return pattern == res
+    except Exception as e:
+        logger.error(str(e))
+        return False
 
 
 def check_re((res, pattern)):
+
     pass
 
 
+def res_parser(res, field):
 
-def res_parser(res,field):
     try:
-        if field in 
+        if field in res:
+            return str(res[field]).strip()
+
+        else:
+            temp_ = field.split('.')
+            for _ in temp_:
+                res = res[_]
+        return str(res).strip()
+
+    except Exception as e:
+        logger.error(str(e))
+
 
