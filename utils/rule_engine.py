55ade8da48ed4be461c8e0c33ec7a4ec226f0109
njcx <njcx86@gmail.com>
2020-04-14 12:40:44 +0800

    init

--- utils/rule_engine.py
+++ utils/rule_engine.py
@@ -3,6 +3,7 @@
 # @Email   : njcx86@gmail.com
 
 from utils.json_ import json_file_to_py
+from utils.check_utils import check_in, check_re, check_equal, res_parser
 from utils.log_ import Logger
 from settings import NidsHome
 logger = Logger.get_logger(__name__)
@@ -11,24 +12,41 @@ logger = Logger.get_logger(__name__)
 class Engine(object):
     def __init__(self, rule_type):
         self.rule_type = rule_type
-        self.rules = self.read_rules()
+        self.rules_func_list= self.rules_to_func_list()
 
-        print self.rule_type
-        print self.rules
 
     def read_rules(self):
         try:
-
             return json_file_to_py("{0}/rules/{1}.json".format(NidsHome, self.rule_type.lower()))
         except Exception as e:
             logger.error(str(e))
             return {}
 
+
+
+
+    def rules_to_func_list(self):
+        try:
+            temp_list = []
+            rules = self.read_rules()
+            for rule in  rules:
+                for detect_item in rule['detect_list']:
+                    print detect_item
+
+                    # temp_list.append(rule)
+
+
+        except Exception as e:
+            logger.error(str(e))
+            return {}
+
+
+
+
     def check_line(self):
 
         pass
 
-    def
 
 
 if __name__ == '__main__':
