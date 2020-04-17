702e260f1b0df08d1cb64d18a1d18c66862269d9
njcx <njcx86@gmail.com>
2020-04-17 17:49:01 +0800

    init

--- rules/icmp_rules.py
+++ rules/icmp_rules.py
@@ -1,32 +1,17 @@
 icmp = [
     {
             "state": "enable",
-            "rule_id": "sqli_get_01",
-            "rule_tag": "sqli",
-            "rule_name": "sqli_get_select",
-            "rule_type": "and",
+            "rule_id": "icmp_01",
+            "rule_tag": "icmp",
+            "rule_name": "icmp",
+            "rule_type": "or",
 
             "detect_list": [
 
               {
-                 "field" : "network.ip",
-                  "type": "re",
-                  "rule":"xxx",
-                  "ignorecase": "False"
-
-              },
-
-              {
-                 "field" : "network.ip",
-                  "type": "equal",
-                  "rule":"xxx"
-              },
-
-
-              {
-                  "field" : "network.ip",
+                  "field" : "icmp.request.message",
                   "type": "in",
-                  "rule":100
+                  "rule": "Unreachable"
               }
 
 
@@ -36,91 +21,5 @@ icmp = [
             "info": "about sql injection attack",
             "e-mail": ["xxx","xxx"]
 
-      },
-
-
-     {
-              "state": "enable",
-              "rule_id": "sqli_get_01",
-              "rule_tag": "sqli",
-              "rule_name": "sqli_get_select",
-              "rule_type": "or",
-
-
-              "detect_list": [
-
-                {
-                 "field" : "network.ip",
-                  "type": "re",
-                  "rule":"xxx",
-                 "ignorecase": "False"
-              },
-
-                {
-                 "field" : "network.ip",
-                  "type": "equal",
-                  "rule":"xxx"
-              },
-
-
-              {
-                  "field" : "network.ip",
-                  "type": "in",
-                  "rule":100
-              }
-
-
-              ],
-              "threat_level":"high",
-              "auth":"njcx",
-              "info": "about sql injection attack",
-              "e-mail": ["xxx","xxx"]
-        },
-
-     {
-              "state": "enable",
-              "rule_id":"sqli_get_01",
-              "rule_tag":"sqli",
-              "rule_name":"sqli_get_select",
-              "rule_type":"frequency",
-
-              "detect_list": [
-                {
-                 "field" : "network.ip",
-                  "type": "re",
-                  "rule": "xxx",
-                  "ignorecase": "False"
-              },
-
-                {
-                 "field" : "network.ip",
-                  "type": "equal",
-                  "rule":"xxx"
-              },
-
-              {
-                  "field" : "network.ip",
-                  "type": "in",
-                  "rule":100
-              }
-
-
-              ],
-
-
-              "key" :["", ""],
-
-              "time_interval":{
-                "second": 10,
-                "times":100
-              },
-
-              "threat_level":"high",
-
-              "auth":"njcx",
-
-              "info": "about sql injection attack",
-              "e-mail": ["xxx","xxx"]
-        }
-
+      }
   ]
--- rules/redis_rules.py
+++ rules/redis_rules.py
@@ -1,32 +1,17 @@
 redis = [
     {
             "state": "enable",
-            "rule_id": "sqli_get_01",
-            "rule_tag": "sqli",
-            "rule_name": "sqli_get_select",
+            "rule_id": "redis_01",
+            "rule_tag": "redis",
+            "rule_name": "redis_auth",
             "rule_type": "and",
 
             "detect_list": [
 
               {
-                 "field" : "network.ip",
-                  "type": "re",
-                  "rule":"xxx",
-                  "ignorecase": "False"
-
-              },
-
-              {
-                 "field" : "network.ip",
-                  "type": "equal",
-                  "rule":"xxx"
-              },
-
-
-              {
-                  "field" : "network.ip",
-                  "type": "in",
-                  "rule":100
+                  "field": "method",
+                  "type":  "in",
+                  "rule": "AUTH"
               }
 
 
@@ -36,91 +21,5 @@ redis = [
             "info": "about sql injection attack",
             "e-mail": ["xxx","xxx"]
 
-      },
-
-
-     {
-              "state": "enable",
-              "rule_id": "sqli_get_01",
-              "rule_tag": "sqli",
-              "rule_name": "sqli_get_select",
-              "rule_type": "or",
-
-
-              "detect_list": [
-
-                {
-                 "field" : "network.ip",
-                  "type": "re",
-                  "rule":"xxx",
-                 "ignorecase": "False"
-              },
-
-                {
-                 "field" : "network.ip",
-                  "type": "equal",
-                  "rule":"xxx"
-              },
-
-
-              {
-                  "field" : "network.ip",
-                  "type": "in",
-                  "rule":100
-              }
-
-
-              ],
-              "threat_level":"high",
-              "auth":"njcx",
-              "info": "about sql injection attack",
-              "e-mail": ["xxx","xxx"]
-        },
-
-     {
-              "state": "enable",
-              "rule_id":"sqli_get_01",
-              "rule_tag":"sqli",
-              "rule_name":"sqli_get_select",
-              "rule_type":"frequency",
-
-              "detect_list": [
-                {
-                 "field" : "network.ip",
-                  "type": "re",
-                  "rule": "xxx",
-                  "ignorecase": "False"
-              },
-
-                {
-                 "field" : "network.ip",
-                  "type": "equal",
-                  "rule":"xxx"
-              },
-
-              {
-                  "field" : "network.ip",
-                  "type": "in",
-                  "rule":100
-              }
-
-
-              ],
-
-
-              "key" :["", ""],
-
-              "time_interval":{
-                "second": 10,
-                "times":100
-              },
-
-              "threat_level":"high",
-
-              "auth":"njcx",
-
-              "info": "about sql injection attack",
-              "e-mail": ["xxx","xxx"]
-        }
-
+      }
   ]
